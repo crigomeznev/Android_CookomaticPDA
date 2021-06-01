1effffeed364cdc898f7c2e78476fcb0cab5515c
crigomeznev <crigomeznev@gmail.com>
2021-06-01 16:54:03 +0200

    refrescar taules funciona

--- app/src/main/java/com/example/cookomaticpda/ServerInfo.java
+++ app/src/main/java/com/example/cookomaticpda/ServerInfo.java
@@ -1,6 +1,7 @@
 package com.example.cookomaticpda;
 
 public class ServerInfo {
-    public static final String SRVIP = "192.168.1.106";
+//    public static final String SRVIP = "192.168.1.106";
+    public static final String SRVIP = "10.132.0.115";
     public static final int SRVPORT = 9876;
 }
